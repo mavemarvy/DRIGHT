0491aa723a7b70f2342da17f78fe47a3bee03e12
Mavemarvy <mvy00342@gmail.com>
2026-08-19 16:51:25 +0100

    feat: support DRIGHT multi-theme tokens

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,6 +1,7 @@
- "use client";
+"use client";
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+
 export function ThemeProvider({children}:{children:React.ReactNode}) {
-  return <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>{children}</NextThemesProvider>;
+  return <NextThemesProvider attribute="class" defaultTheme="system" enableSystem themes={["light","dark","dark-blue","midnight","ocean","slate","high-contrast"]}>{children}</NextThemesProvider>;
 }
