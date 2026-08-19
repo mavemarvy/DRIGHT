ed45c74ca121c4da463550666be8ae5e282e02f5
Mavemarvy <mvy00342@gmail.com>
2026-08-19 08:15:24 +0100

    feat: extend DRIGHT theme provider with selectable themes

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,6 +1,27 @@
- "use client";
+"use client";
+
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-export function ThemeProvider({children}:{children:React.ReactNode}) {
-  return <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>{children}</NextThemesProvider>;
+
+const DRIGHT_THEMES = [
+  "light",
+  "dark",
+  "dark-blue",
+  "midnight",
+  "high-contrast",
+  "system",
+] as const;
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      themes={[...DRIGHT_THEMES]}
+      disableTransitionOnChange
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
