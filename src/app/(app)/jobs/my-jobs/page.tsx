1075ab52570e326466168e1663cd41ff947ab157
Mavemarvy <mvy00342@gmail.com>
2026-08-21 14:48:08 +0100

    feat(jobs): add my jobs route

---
+++ src/app/(app)/jobs/my-jobs/page.tsx
@@ -0,0 +1,5 @@
+import { MyJobsWorkspace } from "@/components/jobs/job-workspaces";
+
+export const metadata = { title: "My Jobs | DRIGHT" };
+
+export default function MyJobsPage() { return <MyJobsWorkspace />; }
