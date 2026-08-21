a60d02c02f901ac7315c6f7075021c67f09324f1
Mavemarvy <mvy00342@gmail.com>
2026-08-21 14:47:48 +0100

    feat(jobs): add post job route

---
+++ src/app/(app)/jobs/post/page.tsx
@@ -0,0 +1,7 @@
+import JobForm from "@/components/jobs/job-form";
+
+export const metadata = { title: "Post a Job | DRIGHT" };
+
+export default function PostJobPage() {
+  return <div className="dright-page"><JobForm /></div>;
+}
